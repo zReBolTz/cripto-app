cf1e39eaa00334c8d5826338af341ccb67564194
zReBolTz <ferreiraw36@gmail.com>
2025-01-20 22:01:43 -0300

    feat: estilizando a lista de moedas

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,6 +1,7 @@
 import { Search } from "lucide-react";
 import style from "./home.module.css";
 import { useState } from "react";
+import { Link } from "react-router-dom";
 const Home = () => {
   const [input, setInput] = useState("");
 
@@ -18,6 +19,38 @@ const Home = () => {
           <Search />
         </button>
       </form>
+      <table>
+        <thead>
+          <tr>
+            <th scope="col">Moeda</th>
+            <th scope="col">Valor Mercado </th>
+            <th scope="col">Preço</th>
+            <th scope="col">Volume</th>
+            <th scope="col">Mudança 24h</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr className={style.tr}>
+            <td className={style.tdLabel} data-label="Moeda">
+              <div className={style.name}>
+                <Link to="/detail/bitcoint">Bitcoin | BTC</Link>
+              </div>
+            </td>
+            <td className={style.tdLabel} data-label="Valor Mercado">
+              1T
+            </td>
+            <td className={style.tdLabel} data-label="Preço">
+              8.000
+            </td>
+            <td className={style.tdLabel} data-label="Volume">
+              2B
+            </td>
+            <td className={style.tdLoss} data-label="Mudança 24h">
+              -0,20
+            </td>
+          </tr>
+        </tbody>
+      </table>
     </main>
   );
 };
